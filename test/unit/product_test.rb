6d85e06cc922ff1eb6efbfa062d00b0048964edd
Nan Sha <iamshanan@gmail.com>
2012-05-13 14:56:38 +0800

    unit test fixture used

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  def new_product(image_url)
+    Product.new(
+        :title => "My Book Title",
+        :description => "yyy",
+        :price => 1,
+        :image_url => image_url
+    )
+  end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -31,4 +41,30 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(
+        :title => products(:ruby).title,
+        :description => "yyy",
+        :price => 1,
+        :image_url => "fred.gif"
+    )
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join(';')
+
+  end
+
 end
